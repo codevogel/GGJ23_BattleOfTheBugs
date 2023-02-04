1ed93c9b5a1c8a51b060d594e042a225232e92b5
Kamiel <kamieldevisser@gmail.com>
2023-02-04 20:54:56 +0100

    add stone obstacles

---
+++ the OG/Assets/Obstacle.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+class Obstacle : MonoBehaviour
+{
+    public float size;
+
+    private void OnDrawGizmos()
+    {
+        //Gizmos.color = Color.magenta;
+        //Gizmos.DrawWireSphere(transform.position, size);
+    }
+
+    private void OnValidate()
+    {
+        GetComponent<CircleCollider2D>().radius = size;
+    }
+}
