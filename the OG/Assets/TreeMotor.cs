c6c214b809eed08a9defc6c9112fe78e6813eff6
Kamiel <kamieldevisser@gmail.com>
2023-02-04 14:36:09 +0100

    add root renderer

--- the OG/Assets/TreeMotor.cs
+++ the OG/Assets/TreeMotor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -11,51 +12,40 @@ public class TreeMotor : MonoBehaviour
     [Range(.01f, 1)]
     private float _brakeFactor;
 
-    private Rigidbody2D _rb2d;
+    private Rigidbody2D b_rb2d;
+    public Rigidbody2D Body { get { return b_rb2d; } private set { b_rb2d = value; } }
 
     private float _rawInput;
-    private bool _isBraking;
 
     private void Awake()
     {
 	    EventManager.OnPlayer1MovePerformed += MoveInDirection;
-	    EventManager.OnPlayer1MoveCanceled += StartBraking;
+	    EventManager.OnPlayer1MoveCanceled += ClearRawInput;
 	}
 
     private void Start()
     {
-        _rb2d = GetComponent<Rigidbody2D>();
+        Body = GetComponent<Rigidbody2D>();
     }
 
     private void FixedUpdate()
     {
-        if (_isBraking)
-        {
-            _rb2d.velocity *= (1 - _brakeFactor);
-            return;
-        }
-        _rb2d.AddForce(new Vector2(_rawInput * _motorSpeed * Time.deltaTime, 0));
+        Body.velocity = new Vector2(_rawInput * _motorSpeed * Time.deltaTime, 0);
     }
 
     public void MoveInDirection(float axisRaw)
     {
-	    StopBraking();
         _rawInput = axisRaw;
-    }
-
-    public void StopBraking()
-    {
-        _isBraking = false;
-    }
-
-    public void StartBraking()
-    {
-        _isBraking = true;
     }
 
     private void OnDestroy()
     {
 	    EventManager.OnPlayer1MovePerformed -= MoveInDirection;
-	    EventManager.OnPlayer1MoveCanceled -= StartBraking;
+	    EventManager.OnPlayer1MoveCanceled -= ClearRawInput;
+    }
+
+    private void ClearRawInput()
+    {
+        _rawInput = 0;
     }
 }
