ccf5fdc2403c4ee3f3cee5205be3345716ab63c6
Kamiel <kamieldevisser@gmail.com>
2023-02-04 19:26:28 +0100

    Clamp root position to ground

--- the OG/Assets/Scripts/Player/Root/RootMotor.cs
+++ the OG/Assets/Scripts/Player/Root/RootMotor.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -25,6 +26,15 @@ public class RootMotor : MonoBehaviour
         if (CanMove)
         {
             _rb2d.velocity = _inputVector * Time.deltaTime * _motorSpeed;
+        }
+        ClampY();
+    }
+
+    private void ClampY()
+    {
+        if (_rb2d.position.y > 0)
+        {
+            _rb2d.position = new Vector2(_rb2d.position.x, 0);
         }
     }
 
