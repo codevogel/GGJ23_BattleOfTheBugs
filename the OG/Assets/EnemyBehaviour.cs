fe75e024121eac5a74a5ea6175d9532f8dd778ac
Feskor <patrick.yel@hotmail.com>
2023-02-04 15:13:40 +0100

    working enemy

--- the OG/Assets/EnemyBehaviour.cs
+++ the OG/Assets/EnemyBehaviour.cs
@@ -5,32 +5,66 @@ using UnityEngine;
 public class EnemyBehaviour : MonoBehaviour
 {
     public float Health = 10f;
-    public float MovementSpeed = 0.5f;
+    public float MovementSpeed = 2f;
     public Transform target;
     private Vector3 targetPos;
     public int scale;
+    public float health = 1f;
+    public float damage = 20f;
+    Coroutine damageRoutine;
 
-    private void Start()
-    {
-
-    }
-
-    private void Awake()
-    {
-        
-        
-    }
 
     private void Update()
     {
-        Vector3 newScale = transform.localScale;
+        Vector3 newScale = transform.GetChild(0).localScale;
         newScale.x = scale;
-        transform.localScale = newScale;
+        transform.GetChild(0).localScale = newScale;
         targetPos = target.transform.position;
         
         Vector3 newXpos = new Vector3(targetPos.x, transform.position.y, transform.position.z);
 
         transform.position = Vector3.MoveTowards(transform.position, newXpos, MovementSpeed * Time.deltaTime);
+
+        if(health <= 0)
+        {
+            Die();
+        }
     }
 
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.transform.tag == "Player")
+        {
+            damageRoutine = StartCoroutine(DamageTree(damage));
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.transform.tag == "Player")
+        {
+            StopCoroutine(damageRoutine);
+        }
+    }
+
+    private IEnumerator DamageTree(float damage)
+    {
+        while (true)
+        {
+            GameManager.Instance.gameObject.GetComponent<Energy>().DecreaseEnergy(damage);
+            yield return new WaitForSeconds(1.5f);
+        }
+        
+    }
+
+    public void TakeDamage(float damage)
+    {
+        health -= damage;
+    }
 }
