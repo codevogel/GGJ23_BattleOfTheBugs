a065f7caed70bae49d59942e6a5796e5975e0252
Jord Leek <jordleek2000@gmail.com>
2023-02-05 01:06:57 +0100

    Completed SonarBehaviour

--- the OG/Assets/Scripts/Player/Root/SonarBehaviour.cs
+++ the OG/Assets/Scripts/Player/Root/SonarBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Claims;
 using UnityEngine;
 
 public class SonarBehaviour : MonoBehaviour
@@ -21,10 +22,12 @@ public class SonarBehaviour : MonoBehaviour
 	private List<Ray2D> m_Rays = new List<Ray2D>();
 
 	private float m_Time;
+	private bool m_NoAim = true;
 
 	private void Awake()
 	{
 		EventManager.OnPlayer2AimPerformed += AimTowards;
+		EventManager.OnPlayer2AimCanceled+= FadePointer;
 		EventManager.OnPlayer2Attack += SpawnSonar;
 	}
 
@@ -39,8 +42,34 @@ public class SonarBehaviour : MonoBehaviour
 	public void AimTowards(Vector2 input)
 	{
 		m_InputVector = input.normalized;
+		StopCoroutine(FadeTo(0.0f, 0.1f));
+		StartCoroutine(FadeTo(1.0f, 0.1f));
+		m_NoAim = true;
 	}
 
+	private void FadePointer()
+	{
+		StopCoroutine(FadeTo(1.0f, 0.1f));
+		StartCoroutine(FadeTo(0.0f, 0.1f));
+		m_NoAim = true;
+
+	}
+	private IEnumerator FadeTo(float aValue, float aTime)
+	{
+		SpriteRenderer[] spriterenders = GetComponentsInChildren<SpriteRenderer>();
+		foreach (SpriteRenderer sprite in spriterenders)
+		{
+			float alpha = sprite.material.color.a;
+			for (float t = 0.0f; t < 1.0f; t += Time.deltaTime / aTime)
+			{
+				Color newColor = new Color(1, 1, 1, Mathf.Lerp(alpha, aValue, t));
+				sprite.material.color = newColor;
+				yield return null;
+			}
+			Color finaleColor = new Color(1, 1, 1, aValue);
+			sprite.material.color = finaleColor;
+		}
+	}
 	private void SpawnSonar()
 	{
 		if (m_Time > 0)
@@ -50,10 +79,10 @@ public class SonarBehaviour : MonoBehaviour
 			m_InputVector = Vector2.down.normalized;
 
 		var rads = Mathf.Atan2(m_InputVector.x, m_InputVector.y);
-		var degrees = rads * Mathf.Rad2Deg - 90;
+		var degrees = rads * Mathf.Rad2Deg + 90;
 
 		var sonarDir = DegreeToVector2(degrees);
-		VisualCueResourceOnRoot.Play(new Vector3(-sonarDir.x, sonarDir.y, 0));
+		VisualCueResourceOnRoot.Play(new Vector3(sonarDir.x, -sonarDir.y, 0));
 		
 		var pos = new Vector2(transform.position.x, transform.position.y);
 
@@ -64,12 +93,13 @@ public class SonarBehaviour : MonoBehaviour
 		for (var i = 0; i < m_RayCount; i++)
 		{
 			var dirVec = DegreeToVector2(degrees + i);
-			m_Rays.Add(new Ray2D(pos, dirVec));
+			m_Rays.Add(new Ray2D(pos, new Vector2(-dirVec.x, dirVec.y)));
 		}
 
 		foreach (var ray in m_Rays)
 		{
 			var hit = Physics2D.Raycast(ray.origin, ray.direction, 100f, LayerMask.GetMask("Obstacle"));
+			Debug.Log($"hit:{hit}");
 			if(hit.collider == null) continue;
 
 			
@@ -93,6 +123,7 @@ public class SonarBehaviour : MonoBehaviour
 	private void OnDestroy()
 	{
 		EventManager.OnPlayer2AimPerformed -= AimTowards;
+		EventManager.OnPlayer2AimCanceled -= FadePointer;
 		EventManager.OnPlayer2Attack -= SpawnSonar;
 	}
 
@@ -101,11 +132,11 @@ public class SonarBehaviour : MonoBehaviour
 		m_Time -= Time.deltaTime;
 	}
 
-	//private void OnDrawGizmos()
-	//{
-	//	foreach (var ray in m_Rays)
-	//	{
-	//		Gizmos.DrawRay(transform.position, new Vector3(ray.direction.x, ray.direction.y, 0));
-	//	}
-	//}
+	private void OnDrawGizmos()
+	{
+		foreach (var ray in m_Rays)
+		{
+			Gizmos.DrawRay(transform.position, new Vector3(ray.direction.x, ray.direction.y, 0));
+		}
+	}
 }
